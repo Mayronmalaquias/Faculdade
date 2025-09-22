deb9b0ad538ea962bcdf6fe92edd0e98a9dbe5eb
Mayronmalaquias <mayronmalaquias1607@gmail.com>
2025-09-22 12:44:55 -0300

    revisao parte 2

--- 6º semestre/revisao.py
+++ 6º semestre/revisao.py
@@ -6,7 +6,7 @@ class GrafoEsparso:
     
     def get_numero_aresta(self):
         return self.numero_aresta
-    
+ 
 
     def get_numero_vertices(self):
         contador = 0
@@ -120,25 +120,58 @@ class GrafoEsparso:
         return True
 
 
-    def is_subgrafo_gerador(self, g1, g2):
-        if g1.get_numero_vertives() != g2.get_numero_vertices():
+    def is_subgrafo_gerador(self, g2):
+        if self.get_numero_vertives() != g2.get_numero_vertices():
             return False
-        for i in g1.lista_adjacencia:
-            for j in g1.lista_adjacencia[i]:
+        for i in self.lista_adjacencia:
+            for j in self.lista_adjacencia[i]:
                 if j in g2.lista_adjacencia[i]:
                     return True
         return False
 
 
     def is_subgrafo_induzido(self, g2):
-        for i in self.get_vertices():
-            if i not in g2.get_vertices():
+        for i in self.get_vertice():
+            if i not in g2.get_vertice():
                 return False
+        
+        vh = self.get_vertice()
         for i in self.lista_adjacencia:
-            for j in self.lista_adjacencia[i]:
-                if j not in g2.lista_adjacencia[i]:
+            for j in g2.lista_adjacencia[i]:
+                if j not in self.lista_adjacencia[i] and j in vh:
+                    return False
+            for k in self.lista_adjacencia[i]:
+                if k not in g2.lista_adjacencia[i] and k not in vh:
                     return False
         return True 
+    
+    def colorir_grafo(self, n_color):
+        if self.get_numero_vertices() < n_color:
+            return True
+                
+        list_color = []
+        for i in range(n_color):
+            list_color.append(i + 1)
+        lista = {}
+        for i in self.get_vertice():
+            lista[i] = 0
+
+        for i in self.get_vertice():
+            flag_color = False
+            lista_color_neigh = []
+            for j in self.lista_adjacencia[i]:
+                if lista[j] != 0:
+                    lista_color_neigh.append(lista[j])
+            for k in list_color:
+                if k not in lista_color_neigh:
+                    lista[i] = k
+                    flag_color = True
+                    break
+            if not flag_color:
+                return False
+        return True
+            
+
 
 
 
@@ -163,9 +196,23 @@ print(g.get_sequencia_de_graus())
 print(g.remover_aresta("B", "C"))
 print(g.get_arestas())
 # print(g.adicionar_aresta("A", "C"))
-# print(g.adicionar_aresta("B", "C"))
+print(g.adicionar_aresta("B", "C"))
+print(g.adicionar_aresta("A", "C"))
 print(g.is_nulo())
 print(g.is_simples())
 print(g.is_complete())
-print
+g2 = GrafoEsparso()
+print(g2.adicionar_no("A"))
+print(g2.adicionar_no("B"))
+print(g2.adicionar_no("C"))
+print(g2.adicionar_no("D"))
+print(g2.adicionar_no("E"))
+print(g2.adicionar_aresta("A", "B"))
+print(g2.adicionar_aresta("B", "C"))
+# print(g2.adicionar_aresta("A", "C"))
+# print(g2.adicionar_aresta("A", "D"))
+# print(g2.adicionar_aresta("C", "D"))
+# print(g2.adicionar_aresta("B", "D"))
+print(g.is_subgrafo_induzido(g2))
+print(g.colorir_grafo(2))
 g.mostrar_lista()
